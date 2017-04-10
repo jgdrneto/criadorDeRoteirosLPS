7fd4884172c9119f3da87b51957aa2c1e4f51be8
Neto Gameleira <jgdrneto@gmail.com>
2017-04-10 19:32:05 -0300

    Adicionado eventos de descanso

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -15,6 +15,7 @@ import construtor.Preferencia;
 import construtor.Preferencia.RANKING;
 import estrategiasDeEscolha.EstrategiaAleatoria;
 import estrategiasDeEscolha.EstrategiaDeEscolha;
+import estrategiasDeEscolha.EstrategiaPorMenorPreco;
 import eventos.Alimentacao;
 import eventos.Descanso;
 import eventos.Exposicao;
@@ -35,19 +36,20 @@ public class Principal {
 		Alimentacao a6 = new Alimentacao("Restaurante do come-come", "Restaurante com melhor self-service da região", "BR 90", 60, new GregorianCalendar(2017, Calendar.MARCH, 10, 12, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 15, 0), QUALIDADE.BOA);
 		Alimentacao a7 = new Alimentacao("Restaurante do dourado", "Restaurante de frutos do mar", "BR 1", 40, new GregorianCalendar(2017, Calendar.MARCH, 10, 12, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 14, 0), QUALIDADE.MEDIA);
 		Alimentacao a8 = new Alimentacao("Restaurante Rei do gado", "Restaurante com o melhor da carne bovina", "BR 101", 50, new GregorianCalendar(2017, Calendar.MARCH, 10, 11, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 14, 0), QUALIDADE.MEDIA);
-		Alimentacao a9 = new Alimentacao("Restaurante Bom ponto", "Restaurante com um excelente local de acesso", "BR 101", 40, new GregorianCalendar(2017, Calendar.MARCH, 10, 13, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 15, 0), QUALIDADE.BOA);
+		Alimentacao a9 = new Alimentacao("Restaurante Bom ponto", "Restaurante com um excelente local de acesso", "BR 101", 40, new GregorianCalendar(2017, Calendar.MARCH, 10, 14, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 15, 0), QUALIDADE.BOA);
 		Alimentacao a10 = new Alimentacao("Restaurante do Buffalo Bill", "Restaurante especializado em comida de buffalo", "BR 101", 150, new GregorianCalendar(2017, Calendar.MARCH, 10, 14, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 15, 0), QUALIDADE.EXCELENTE);
 		
 		Descanso d1 = new Descanso("Hotel sono bom", "Hotel para sono top", "Rua do Malte", 80, new GregorianCalendar(2017, Calendar.MARCH, 10, 22, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 23, 0), QUALIDADE.MEDIA);
-		/*
-		Descanso d2 = new Descanso("Restaurante do Guiné", "Restaurante com comida caseira do sertão", "BR 101", 75, new GregorianCalendar(2017, Calendar.MARCH, 10, 11, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 14, 0), QUALIDADE.MEDIA);
-		Descanso d4 = new Descanso("Restaurante do Guiné", "Restaurante com comida caseira do sertão", "BR 101", 75, new GregorianCalendar(2017, Calendar.MARCH, 10, 11, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 14, 0), QUALIDADE.MEDIA);
-		Descanso d5 = new Descanso("Restaurante do Guiné", "Restaurante com comida caseira do sertão", "BR 101", 75, new GregorianCalendar(2017, Calendar.MARCH, 10, 11, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 14, 0), QUALIDADE.MEDIA);
-		Descanso d6 = new Descanso("Restaurante do Guiné", "Restaurante com comida caseira do sertão", "BR 101", 75, new GregorianCalendar(2017, Calendar.MARCH, 10, 11, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 14, 0), QUALIDADE.MEDIA);
-		Descanso d7 = new Descanso("Restaurante do Guiné", "Restaurante com comida caseira do sertão", "BR 101", 75, new GregorianCalendar(2017, Calendar.MARCH, 10, 11, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 14, 0), QUALIDADE.MEDIA);
-		Descanso d8 = new Descanso("Restaurante do Guiné", "Restaurante com comida caseira do sertão", "BR 101", 75, new GregorianCalendar(2017, Calendar.MARCH, 10, 11, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 14, 0), QUALIDADE.MEDIA);
-		Descanso d9 = new Descanso("Restaurante do Guiné", "Restaurante com comida caseira do sertão", "BR 101", 75, new GregorianCalendar(2017, Calendar.MARCH, 10, 11, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 14, 0), QUALIDADE.MEDIA);
-		*/
+		Descanso d2 = new Descanso("Pousada soneca", "Pousada a brisa mar", "Rua da Pestana", 50, new GregorianCalendar(2017, Calendar.MARCH, 10, 20, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 23, 0), QUALIDADE.BOA);
+		Descanso d3 = new Descanso("Hotel do João Pestana", "Hotel de primeira quailidade", "Rua da soneca", 150, new GregorianCalendar(2017, Calendar.MARCH, 10, 18, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 23, 0), QUALIDADE.EXCELENTE);
+		Descanso d4 = new Descanso("Hertz Center Hotel", "O hotel mais luxuoso da cidade", "Rua São Manuel", 200, new GregorianCalendar(2017, Calendar.MARCH, 10, 18, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 23, 0), QUALIDADE.MEDIA);
+		Descanso d5 = new Descanso("Pousada da caridade", "Pousada boa, bonita e barata", "Rua João Catinguera", 30, new GregorianCalendar(2017, Calendar.MARCH, 10, 18, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 20, 0), QUALIDADE.BOA);
+		Descanso d6 = new Descanso("Hotel do conhecimento", "Durma aprendendo", "Rua do saber", 90, new GregorianCalendar(2017, Calendar.MARCH, 10, 19, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 21, 0), QUALIDADE.MEDIA);
+		Descanso d7 = new Descanso("Hotel Colina", "Hotel com bela vista para a cidade", "Rua do Monte Azul", 80, new GregorianCalendar(2017, Calendar.MARCH, 10, 20, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 23, 0), QUALIDADE.MEDIA);
+		Descanso d8 = new Descanso("Natal Palace Hotel", "O Melhor hotel do bairro alecrim", "Avenida Brasil", 75, new GregorianCalendar(2017, Calendar.MARCH, 10, 21, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 22, 0), QUALIDADE.MEDIA);
+		Descanso d9 = new Descanso("Hotel boa dormida", "Melhor local para descanso para você", "Rua do sono", 70, new GregorianCalendar(2017, Calendar.MARCH, 10, 19, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 21, 0), QUALIDADE.MEDIA);
+		Descanso d10 = new Descanso("Hotel Ibernação", "Aqui vc dorme de verdade", "Rua do Inverno", 85, new GregorianCalendar(2017, Calendar.MARCH, 10, 21, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 23, 0), QUALIDADE.MEDIA);
+		
 		/*
 		Alimentacao d10 = new Alimentacao("Restaurante do Guiné", "Restaurante com comida caseira do sertão", "BR 101", 75, new GregorianCalendar(2017, Calendar.MARCH, 10, 11, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 14, 0), QUALIDADE.MEDIA);
 		Alimentacao c3 = new Alimentacao("Restaurante do Guiné", "Restaurante com comida caseira do sertão", "BR 101", 75, new GregorianCalendar(2017, Calendar.MARCH, 10, 11, 0), new GregorianCalendar(2017, Calendar.MARCH, 10, 14, 0), QUALIDADE.MEDIA);
@@ -81,6 +83,15 @@ public class Principal {
 		eventos.add(a9.converterParaJSON());
 		eventos.add(a10.converterParaJSON());
 		eventos.add(d1.converterParaJSON());
+		eventos.add(d2.converterParaJSON());
+		eventos.add(d3.converterParaJSON());		
+		eventos.add(d4.converterParaJSON());
+		eventos.add(d5.converterParaJSON());
+		eventos.add(d6.converterParaJSON());
+		eventos.add(d7.converterParaJSON());
+		eventos.add(d8.converterParaJSON());
+		eventos.add(d9.converterParaJSON());
+		eventos.add(d10.converterParaJSON());
 		
 		List<Preferencia> preferencias = new ArrayList<Preferencia>();
 		
@@ -102,11 +113,12 @@ public class Principal {
 			
 			ConstrutorDeRoteiro construtor = new ConstrutorDeRoteiro(le, classes, preferencias, 2017, Calendar.MARCH, 10,7);
 			
-			EstrategiaDeEscolha eAleatoria = new EstrategiaAleatoria();
+			//EstrategiaDeEscolha estrategia = new EstrategiaAleatoria();
+			EstrategiaDeEscolha estrategia = new EstrategiaPorMenorPreco();
+			 
+			List<Evento> roteiro = construtor.getRoteiro(estrategia);
 			
-			List<Evento> roteiro = construtor.getRoteiro(eAleatoria); 
-			
-			List<List<Evento>> matrizRoteiro = construtor.selecionarRoteirosPossiveis();
+			//List<List<Evento>> matrizRoteiro = construtor.selecionarRoteirosPossiveis();
 			
 			/*
 			for(List<Evento>lme : matrizRoteiro){
